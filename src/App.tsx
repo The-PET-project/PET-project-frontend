c2b96c98c0f603ddd7bebdf58cf4d7b1dc7b82a0
Redturtle42 <izabella.tanacs@yahoo.com>
2023-01-17 22:51:56 +0100

    PPF-001: setup base react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+const App: React.FC = () => {
+  return (
+    <div className="wrapper">
+      <h1>Pet Project</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,55 @@
+import path from "path";
+import { Configuration as WebpackConfiguration, DefinePlugin } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const webpackConfig = (): Configuration => ({
+  entry: "./src/index.tsx",
+  ...(process.env.production || !process.env.development
+    ? {}
+    : { devtool: "eval-source-map" }),
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
+  },
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "build.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+        exclude: /build/,
+      },
+      {
+        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.scss$/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles
+      template: "./public/index.html",
+    }),
+    // DefinePlugin allows you to create global constants which can be configured at compile time
+    new DefinePlugin({
+      "process.env": process.env.production || !process.env.development,
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+});
+
+export default webpackConfig;
